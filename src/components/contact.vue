bbb98193ab92cd5ed87f753523d1e590ac3fdab4
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-08-09 20:59:38 +0100

    updated the contact page

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,55 +1,22 @@
-
 <template>
     <div>
         <div class="container" style="padding:10%">
             <h2>Contact Me</h2>
-            <form action="oyetubobamiji@gmail.com" 
-              method="post" enctype="text/plain">
+            <form name="contact" action=""  method="POST" >
+                <input type="hidden" name="form-name" value="contact" />
                 <div class="form-group">
-                    <input type="email" class="form-control"
-                      v-model="contact.email" placeholder="Enter email">
+                    <input type="email" class="form-control" name="email"
+                       placeholder="Enter email">
                 </div>
                 <div class="form-group">
-                    <textarea class="form-control" 
-                    v-model="contact.message" rows="3" placeholder="write message">
+                    <textarea class="form-control" name="message"
+                   rows="3" placeholder="write message">
                     </textarea>
                 </div>
+                  <input class="form-button" type="submit" value="Send message" />
             </form>
             
-            <button type="button" @click="submit" class="btn btn-primary btn-lg">
-                Contact
-            </button>
+            
         </div>
     </div>
 </template>
-
-
-<script lang="ts">
-
-export default {
-    name:'contact',
-
-    data(){
-        return {
-            contact:{
-                email:'',
-                message:''
-            }
-        }
-    },
-    
-    methods:{
-        submit(){
-            //validating the form
-            if(this.contact.email !="" && this.contact.message != ""){
-            alert('Thank You')
-            
-            }else{
-                alert("All fields are required");
-            }
-            
-        }
-    }
-}
-
-</script>

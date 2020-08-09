1d05a61c9b9d53ead895ccec235690e3e91a80c8
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-08-09 21:31:11 +0100

    updated links

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,31 +1,27 @@
 <template>
   <div>
-       <b-navbar toggleable="lg" type="dark" variant="dark" class="navv">
-    <b-navbar-brand href="#">Home</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="dark" class="navv">
+      <b-navbar-brand>   <router-link to="/">Home</router-link></b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">About</b-nav-item>
-        <b-nav-item href="#" >Contact</b-nav-item>
-        <b-nav-item href="#" >portfolio</b-nav-item>
-      </b-navbar-nav>
-
-    
-      
-    </b-collapse>
-  </b-navbar> 
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link>Portfolio</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link>Contact</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
-
 </style>

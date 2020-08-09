42e5f19e384d1295e881691ed288843791ddfb34
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-08-09 21:18:22 +0100

    updated contact form

--- src/router.js
+++ src/router.js
@@ -1,10 +1,18 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 Vue.use(VueRouter);
 let router = new VueRouter({
-    routes:[
-        {path:'/',  name:'Landing', component: () =>import('./components/Lander')},
-        
-    ]
+  routes: [
+    {
+      path: "/",
+      name: "Landing",
+      component: () => import("./components/Lander"),
+    },
+    {
+      path: "/thank-you",
+      name: "Thank You",
+      component: () => import("./components/thank-you"),
+    },
+  ],
 });
 export default router;

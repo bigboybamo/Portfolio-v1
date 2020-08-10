c089814090aeb89075da81b7fb01be9ffa32ee5d
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-08-10 22:09:58 +0100

    added router

--- src/router.js
+++ src/router.js
@@ -1,18 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Lander from "./components/Lander.vue";
+import thankyou from "./components/thank-you.vue";
+
 Vue.use(VueRouter);
-let router = new VueRouter({
-  routes: [
-    {
-      path: "/",
-      name: "Landing",
-      component: () => import("./components/Lander"),
-    },
-    {
-      path: "/thank-you",
-      name: "Thank You",
-      component: () => import("./components/thank-you"),
-    },
-  ],
+
+const routes = [
+  {
+    path: "/",
+    name: "Lander",
+    component: Lander,
+  },
+  {
+    path: "/thank-you",
+    name: "thankyou",
+    component: thankyou,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
+
 export default router;
